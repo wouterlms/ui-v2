f4fb46b24f1b6e25c21f7b8fcede44fdbcd17304
Wouter <wouter.laermans@appwise.be>
2022-10-26 20:39:22 +0200

    remove icons option from theme

--- src/theme/createTheme.ts
+++ src/theme/createTheme.ts
@@ -3,27 +3,18 @@ import { watchEffect } from 'vue'
 import type { ColorConfig } from '../types'
 import { extendColors, setCssVariables } from './utils'
 
-import { icons } from './icons'
 import { useDarkMode } from '@/composables'
 
 interface Options {
   enableDarkMode: boolean
-  icons: Record<string, string>
   colors: ColorConfig
 }
 
 const { detectTheme } = useDarkMode()
 
-export default ({ enableDarkMode, colors, icons: _icons }: Options): void => {
+export default ({ enableDarkMode, colors }: Options): void => {
   if (enableDarkMode)
     detectTheme()
-
-  Object.entries(_icons).forEach(([
-    key,
-    value,
-  ]) => {
-    icons[key] = value
-  })
 
   extendColors(colors)
 
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,5 +1,4 @@
 export { default as baseColors } from './baseColors'
 export * from './colors'
 export { default as createTheme } from './createTheme'
-export * from './icons'
 export * from './utils'
